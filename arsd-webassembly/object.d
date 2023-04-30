c918f73cce14bd6fd8c08d4ea3c80fb85067f10f
Hipreme <msnmancini@hotmail.com>
2023-04-30 11:11:52 -0300

    Fix memcpy commented for wasm

--- arsd-webassembly/object.d
+++ arsd-webassembly/object.d
@@ -125,14 +125,16 @@ extern(C) void* memset(void* s, int c, size_t n)  @nogc nothrow pure
 pragma(LDC_intrinsic, "llvm.memcpy.p0i8.p0i8.i#")
     void llvm_memcpy(T)(void* dst, const(void)* src, T len, bool volatile_ = false);
 
+version(WebAssembly)
+extern(C) void *memcpy(void* dest, const(void)* src, size_t n) pure @nogc nothrow
+{
+	ubyte *d = cast(ubyte*) dest;
+	const (ubyte) *s = cast(const(ubyte)*)src;
+	for (; n; n--) *d++ = *s++;
+	return dest;
+}
+else
 extern(C) void *memcpy(void* dest, const(void)* src, size_t n) pure @nogc nothrow;
-// extern(C) void *memcpy(void* dest, const(void)* src, size_t n) pure @nogc nothrow
-// {
-// 	ubyte *d = cast(ubyte*) dest;
-// 	const (ubyte) *s = cast(const(ubyte)*)src;
-// 	for (; n; n--) *d++ = *s++;
-// 	return dest;
-// }
 
 extern(C) int memcmp(const(void)* s1, const(void*) s2, size_t n) pure @nogc nothrow @trusted
 {
